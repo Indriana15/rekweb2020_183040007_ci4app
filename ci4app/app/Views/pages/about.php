d7509ccaff95bb3e89d5ef9a501936618b33628d
Indriana15 <indrianapuspasaladina@gmail.com>
2020-10-28 22:30:35 +0700

    ci4app

--- ci4app/app/Views/pages/about.php
+++
@@ -1,8 +0,0 @@
-<div class="container">
-    <div class="row">
-        <div class="col">
-            <h1>About Me</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia magni accusantium labore consequuntur veritatis error tenetur animi dolore quibusdam, rem, sapiente facere modi molestias fuga, minima quidem! Sunt, distinctio. Mollitia!</p>
-        </div>
-    </div>
-</div>
